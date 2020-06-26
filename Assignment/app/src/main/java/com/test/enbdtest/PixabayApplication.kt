a8b261a247061d864e006eb5001cef988150d0b4
Saifuddin.Zeenwala <szeenwala@gmail.com>
2020-06-26 12:18:56 +0400

    pushed missing files

---
+++ Assignment/app/src/main/java/com/test/enbdtest/PixabayApplication.kt
@@ -0,0 +1,19 @@
+package com.test.enbdtest
+
+import com.test.enbdtest.di.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+/**
+ * The Base Application which will live entire lifecycle of an application.
+ *
+ * I want to make our AppComponent live in the entire lifecycler of an
+ * application so we can instantiate it on applicationInjection
+ * overrided method which is comming from [DaggerApplication]
+ */
+class PixabayApplication : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
+        return DaggerAppComponent.builder().application(this).build()
+    }
+}
