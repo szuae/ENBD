9b97856033da9b565e750f09172a015a7acc0112
Saifuddin.Zeenwala <szeenwala@gmail.com>
2020-06-26 09:00:58 +0400

    extra files

---
+++ Assignment/app/src/main/java/com/test/enbdtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.enbdtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
